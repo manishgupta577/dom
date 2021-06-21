5a4ce228b53900d2d695ca3d363aff0c922d8fc6
manishgupta577 <67793046+manishgupta577@users.noreply.github.com>
2021-06-22 00:25:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import Project from "./components/pages/Project";
+import ScrollToTop from "./components/inc/ScrollToTop";
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 
@@ -15,7 +16,7 @@ import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 function App() {
   return (
     <Router>
-      
+     <ScrollToTop/>  
     <div>
      <Navbar1/>
      <Switch>
